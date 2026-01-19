dd84e5e030c9738690f5bf7a80e03c7a751cb6fc
stellaleslie45-dot <stellaleslie45@gmail.com>
2026-01-19 16:04:21 +0100

    ajout de la création des nouveaux profil

--- donnees.py
+++ donnees.py
@@ -6,3 +6,7 @@ DOSSIER_DATA = "data"
 DOSSIER_PROFILS = os.path.join(DOSSIER_DATA, "profils")
 FICHIER_CLASSEMENTS = os.path.join(DOSSIER_DATA, "classements.json")
 
+# Création automatique du dossier profils s'il n'existe pas
+if not os.path.exists(DOSSIER_PROFILS):
+    os.makedirs(DOSSIER_PROFILS, exist_ok=True)
+
