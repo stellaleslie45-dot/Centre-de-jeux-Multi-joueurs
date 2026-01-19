913bb41e2541614d4b2cd006a47c45b6db86e2dc
jatsa <jatsacecilia858@gmail.com>
2026-01-19 15:53:41 +0100

    ajout sauvegarde profil

--- gestion_profils.py
+++ gestion_profils.py
@@ -4,3 +4,13 @@ import os
 from datetime import datetime
 import donnees 
 
+def sauvegarder(profil):
+    nom_fichier = f"{profil['nom']}.json"
+    chemin_complet = os.path.join(donnees.DOSSIER_PROFILS, nom_fichier)
+
+    try:
+        with open(chemin_complet, 'w', encoding='utf-8') as f:
+            json.dump(profil, f, indent=4, ensure_ascii=False)
+    except IOError as e:
+        print(f"Erreur lors de la sauvegarde du profil : {e}")
+
