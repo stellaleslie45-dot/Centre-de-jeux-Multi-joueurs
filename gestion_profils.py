f20c67ed33c6fe6cbef90f421ebb8b017da373f7
jatsa <jatsacecilia858@gmail.com>
2026-01-19 15:56:49 +0100

    ajout creer_profil

--- gestion_profils.py
+++ gestion_profils.py
@@ -14,3 +14,34 @@ def sauvegarder(profil):
     except IOError as e:
         print(f"Erreur lors de la sauvegarde du profil : {e}")
 
+def creer_profil():
+    print("\n--- CRÉATION DE PROFIL ---")
+    while True:
+        nom = input("Entrez votre nom de joueur (lettres/chiffres seulement) : ").strip()
+        
+        if not nom.isalnum():
+            print("Le nom ne doit contenir que des lettres et des chiffres.")
+            continue
+        if len(nom) < 3:
+            print("Le nom doit faire au moins 3 caractères.")
+            continue
+            
+        chemin_fichier = os.path.join(donnees.DOSSIER_PROFILS, f"{nom}.json")
+        if os.path.exists(chemin_fichier):
+            print(f"Le profil '{nom}' existe déjà. Choisissez un autre nom.")
+            continue
+            
+        break
+
+    nouveau_profil = {
+        "nom": nom,
+        "date_creation": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "parties": [],
+        "score_total": 0,
+        "succes": []
+    }
+
+    sauvegarder(nouveau_profil)
+    print(f"Profil '{nom}' créé avec succès !")
+    return nouveau_profil
+
