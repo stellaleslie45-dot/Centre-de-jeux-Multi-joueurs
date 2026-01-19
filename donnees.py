9fcd9a59cf5c22333ff7dd5d9a7345d3003c5194
stellaleslie45-dot <stellaleslie45@gmail.com>
2026-01-19 15:58:35 +0100

    ajout de la configuratiuon des dossiers

--- donnees.py
+++ donnees.py
@@ -1,3 +1,8 @@
 # Fichier: donnees.py
 import os
 
+# --- CONFIGURATION DES DOSSIERS ---
+DOSSIER_DATA = "data"
+DOSSIER_PROFILS = os.path.join(DOSSIER_DATA, "profils")
+FICHIER_CLASSEMENTS = os.path.join(DOSSIER_DATA, "classements.json")
+
